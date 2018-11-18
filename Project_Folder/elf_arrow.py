e912a7895501e15b76009ada3ddc677949430302
ruen_mainpc <ruen346@gmail.com>
2018-11-18 16:41:14 +0900

    캐릭터가 쏘는 화살 클레스 생성

---
+++ Project_Folder/elf_arrow.py
@@ -0,0 +1,19 @@
+from pico2d import *
+import game_world
+
+class Elf_arrow:
+    image = None
+
+    def __init__(self, x, y, x_vector, y_vector):
+        self.image = load_image('ball21x21.png')
+        self.x, self.y, self.x_vector, self.y_vector = x, y, x_vector, y_vector
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        self.x += self.x_vector
+        self.y += self.y_vector
+
+        if self.x < 25 or self.x > 1600 - 25:
+            game_world.remove_object(self)
