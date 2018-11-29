1032cb421feb5278169a6e83fd32418bfa961c2d
Ruen_note <ruen346@gmail.com>
2018-11-30 00:48:29 +0900

    중간보스 변수명 변경

--- Project_Folder/boss.py
+++ Project_Folder/boss.py
@@ -17,69 +17,69 @@ FRAMES_PER_ACTION = 8
 class IdleState:
 
     @staticmethod
-    def enter(teemo, event):
-        teemo.timer = get_time()
+    def enter(boss, event):
+        boss.timer = get_time()
 
     @staticmethod
-    def exit(teemo, event):
+    def exit(boss, event):
         pass
 
     @staticmethod
-    def do(teemo):
-        if teemo.move == 1 and teemo.x >= 128 * 6 - 64:
-            teemo.move = 2
-        elif teemo.move == 2 and teemo.y <= 720 - (128 * 10 - 64):
-            teemo.move = 3
-        elif teemo.move == 3 and teemo.x >= 128 * 18 - 64:
-            teemo.move = 4
-        elif teemo.move == 4 and teemo.y >= 720 - (128 * 7 - 64):
-            teemo.move = 5
-        elif teemo.move == 5 and teemo.x <= 128 * 12 - 64:
-            teemo.move = 6
+    def do(boss):
+        if boss.move == 1 and boss.x >= 128 * 6 - 64:
+            boss.move = 2
+        elif boss.move == 2 and boss.y <= 720 - (128 * 10 - 64):
+            boss.move = 3
+        elif boss.move == 3 and boss.x >= 128 * 18 - 64:
+            boss.move = 4
+        elif boss.move == 4 and boss.y >= 720 - (128 * 7 - 64):
+            boss.move = 5
+        elif boss.move == 5 and boss.x <= 128 * 12 - 64:
+            boss.move = 6
 
-        if teemo.move == 1:
-            teemo.x += 1.5
-        elif teemo.move == 2:
-            teemo.y -= 1.5
-        elif teemo.move == 3:
-            teemo.x += 1.5
-        elif teemo.move == 4:
-            teemo.y += 1.5
-        elif teemo.move == 5:
-            teemo.x -= 1.5
-        elif teemo.move == 6:
-            teemo.y += 1.5
+        if boss.move == 1:
+            boss.x += 1.5
+        elif boss.move == 2:
+            boss.y -= 1.5
+        elif boss.move == 3:
+            boss.x += 1.5
+        elif boss.move == 4:
+            boss.y += 1.5
+        elif boss.move == 5:
+            boss.x -= 1.5
+        elif boss.move == 6:
+            boss.y += 1.5
 
         for game_object in game_world.all_objects():
             if str(game_object).find("shot_arrow") != -1: # shot_arrow와 충돌시
-                if game_object.x > teemo.x - 64 and game_object.x < teemo.x + 64 and game_object.y < teemo.y + 64 and  game_object.y > teemo.y - 64:
+                if game_object.x > boss.x - 64 and game_object.x < boss.x + 64 and game_object.y < boss.y + 64 and  game_object.y > boss.y - 64:
                     game_world.remove_object(game_object)
-                    teemo.hp -= 40
+                    boss.hp -= 40
                     break
             elif str(game_object).find("elf_arrow") != -1: # elf_arrow와 충돌시
-                if game_object.x > teemo.x - 64 and game_object.x < teemo.x + 64 and game_object.y < teemo.y + 64 and  game_object.y > teemo.y - 64:
+                if game_object.x > boss.x - 64 and game_object.x < boss.x + 64 and game_object.y < boss.y + 64 and  game_object.y > boss.y - 64:
                     game_world.remove_object(game_object)
-                    teemo.hp -= 40
+                    boss.hp -= 40
                     break
 
-        if teemo.hp <= 0: #피가 0되서 죽음
-            game_world.remove_object(teemo)
+        if boss.hp <= 0: #피가 0되서 죽음
+            game_world.remove_object(boss)
             main_state.ui.money += 15
 
-        if teemo.y > 720 + 64: #경로에 나가서 사라짐
-            game_world.remove_object(teemo)
+        if boss.y > 720 + 64: #경로에 나가서 사라짐
+            game_world.remove_object(boss)
             main_state.ui.life -= 1
 
     @staticmethod
-    def draw(teemo):
-        teemo.image.draw(teemo.x + main_state.elf_move_window_x, teemo.y + main_state.elf_move_window_y)
+    def draw(boss):
+        boss.image.draw(boss.x + main_state.elf_move_window_x, boss.y + main_state.elf_move_window_y)
 
 
-class Teemo:
+class Boss:
 
     def __init__(self):
         self.x, self.y = 0, 720-320
-        self.image = load_image('image\\teemo_sp_0.png')
+        self.image = load_image('image\\monster1.png')
         self.move = 1
         self.hp = 800
         self.event_que = []
