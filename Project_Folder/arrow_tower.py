a9b8d139f093d55591912ceab9a17bdd8d240865
ruen_mainpc <ruen346@gmail.com>
2018-12-01 22:41:31 +0900

    타워1이 몬스터1만 인식하는게 아닌 모든 몬스터 인식하게 변경

--- Project_Folder/arrow_tower.py
+++ Project_Folder/arrow_tower.py
@@ -17,7 +17,7 @@ class Arrow_tower:
         front_monster_y = 720  # 맨앞 몬스터 좌표
         front_monster_move = 0 # 맨앞 몬스터 어디경로 이동
         for game_object in game_world.all_objects(): #맨앞 몬스터 위치
-            if str(game_object).find("monster1") != -1:
+            if str(game_object).find("monster1") != -1 or str(game_object).find("monster2") != -1 or str(game_object).find("monster3") != -1 or str(game_object).find("monster4") != -1 or str(game_object).find("boss") != -1 or str(game_object).find("teemo") != -1:
                 if math.sqrt((game_object.x - self.x)**2 + (game_object.y - self.y)**2) < 450:
                     if game_object.move > front_monster_move:
                         front_monster_x = game_object.x
