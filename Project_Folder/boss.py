d0e87961aeccd5158f69d42975b4e4a1229d738d
ruen_mainpc <ruen346@gmail.com>
2018-12-03 04:02:45 +0900

    ㄷ

--- Project_Folder/boss.py
+++ Project_Folder/boss.py
@@ -65,6 +65,18 @@ class IdleState:
                 if math.sqrt((game_object.x - boss.x)**2 + (game_object.y - boss.y)**2) < 250 and get_time() >= boss.time + 0.1:
                     boss.hp -= 20
                     break
+            elif str(game_object).find("boom") != -1: # boom와 충돌시
+                if game_object.x > boss.x - 64 and game_object.x < boss.x + 64 and game_object.y < boss.y + 64 and  game_object.y > boss.y - 64:
+                    game_world.remove_object(game_object)
+                    from fire import Fire
+                    fire = Fire(boss.x, boss.y)
+                    game_world.add_object(fire, 2)
+                    break
+            elif str(game_object).find("fire") != -1: # fire와 충돌시
+                if math.sqrt((game_object.x - boss.x)**2 + (game_object.y - boss.y)**2) < 100 and get_time() >= boss.time + 0.1:
+                    game_world.remove_object(game_object)
+                    boss.hp -= 30
+                    break
 
         if get_time() >= boss.time + 0.1: #다단히트 스킬땜시
             boss.time = get_time()
