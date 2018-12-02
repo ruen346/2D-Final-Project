e973e4803fa73778c9cbf31ee94764365c8e3e6b
ruen_mainpc <ruen346@gmail.com>
2018-12-03 02:28:30 +0900

    마법타워 공격추가

--- Project_Folder/magic.py
+++ Project_Folder/magic.py
@@ -5,8 +5,8 @@ import main_state
 class Magic:
     image = None
 
-    def __init__(self, x, y, x_vector, y_vector):
-        self.image = load_image('image\\magic.png')
+    def __init__(self, x, y):
+        self.image = load_image('image\\tower2_skill.png')
         self.x, self.y = x, y
         self.time = get_time()
 
@@ -14,5 +14,5 @@ class Magic:
         self.image.draw(self.x + main_state.elf_move_window_x, self.y + main_state.elf_move_window_y)
 
     def update(self):
-        if self.time > get_time() - 0.5:
+        if self.time < get_time() - 0.5:
             game_world.remove_object(self)
--- Project_Folder/magic_tower.py
+++ Project_Folder/magic_tower.py
@@ -2,7 +2,7 @@ from pico2d import *
 import game_world
 import main_state
 import math
-from shot_arrow import Shot_arrow
+from magic import Magic
 
 
 class Magic_tower:
@@ -13,22 +13,15 @@ class Magic_tower:
         self.time = get_time()
 
     def update(self):
-        front_monster_x = 0  # 맨앞 몬스터 좌표
         front_monster_y = 720  # 맨앞 몬스터 좌표
-        front_monster_move = 0 # 맨앞 몬스터 어디경로 이동
         for game_object in game_world.all_objects(): #맨앞 몬스터 위치
             if str(game_object).find("monster1") != -1 or str(game_object).find("monster2") != -1 or str(game_object).find("monster3") != -1 or str(game_object).find("monster4") != -1 or str(game_object).find("boss") != -1 or str(game_object).find("teemo") != -1:
-                if math.sqrt((game_object.x - self.x)**2 + (game_object.y - self.y)**2) < 450:
-                    if game_object.move > front_monster_move:
-                        front_monster_x = game_object.x
-                        front_monster_y = game_object.y
+                if math.sqrt((game_object.x - self.x)**2 + (game_object.y - self.y)**2) < 250:
+                    if get_time() >= self.time + 1.5:  # 마법 사용
+                        magic = Magic(self.x, self.y)
+                        game_world.add_object(magic, 2)
+                        self.time = get_time()
 
-        if get_time() >= self.time + 1: #화살발사
-            if front_monster_y != 720: #없으면 화살 발사 x
-                vector = (abs(front_monster_x - self.x) + abs(self.y - front_monster_y)) / 25
-                shot_arrow = Shot_arrow(self.x, self.y, (front_monster_x - self.x) / vector, (front_monster_y - self.y) / vector)
-                game_world.add_object(shot_arrow, 2)
-            self.time = get_time()
 
     def draw(self):
         self.image.draw(self.x + main_state.elf_move_window_x, self.y + main_state.elf_move_window_y)
